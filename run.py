20e6ed2cbe5588888bb16a3d9e51946979d30dc7
Akhil Senthil <akhilsenthil696@gmail.com>
2026-08-18 20:35:44 +0530

    chore(server): disable auto-reloader in local development runner

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     print(" Access Swagger API:    http://localhost:5000/apidocs")
     print(" Access Health Probes:  http://localhost:5000/health")
     print("=======================================================\n")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
